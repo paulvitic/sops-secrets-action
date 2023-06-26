6e495469b9266bef1eade43819f7741398e0c771
paulvitic <paul.vitic@gmail.com>
2023-06-26 09:03:48 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import { execSync } from 'child_process';
+import {installSops} from "./installSops";
+
+function isSopsInstalled(): boolean {
+  try {
+    execSync(`sops -v`);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+async function run(): Promise<void> {
+  try {
+
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+    //
+    // core.setOutput('time', new Date().toTimeString())
+
+    if (!isSopsInstalled()) {
+      await installSops();
+    } else {
+      console.log(`sops is already installed.`);
+    }
+
+    const sopsPath: string = core.getInput('sopsPath')
+    core.debug(`Waiting ${sopsPath} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG`
+
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run().catch(core.setFailed)
